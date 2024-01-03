dc7e6d366319becfb97a339c7fbe7fe615e4c3a4
Sameh ElGazar <selgazar95@gmail.com>
2024-01-03 11:45:33 -0800

    Advanced

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,7 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/127.0.0.[0-9]/).join
+from = ARGV[0].scan(/\[from:(\S+)\]/).flatten.first
+to = ARGV[0].scan(/\[to:(\S+)\]/).flatten.first
+flags = ARGV[0].scan(/\[flags:(\S+)\]/).flatten.first
+
+puts "#{from},#{to},#{flags}"
+
