768166b6bd392eb62a2e3f618a1715b4063a8857
Sameh ElGazar <selgazar95@gmail.com>
2024-02-24 00:33:10 +0200

    Task 2 wekzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,17 @@
 # Puppet manifest to install flask
 
-package{ 'python':
+package{ 'python3':
   ensure => '3.8.10'
 }
+
 package{ 'werkzeug':
-  ensure => '2.1.1',
+  ensure  => '2.1.1',
+  require => Package['python3']
 }
 
 package{ 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
-  require  => Package['python', 'werkzeug']
+  require  => Package['werkzeug']
 }
 
