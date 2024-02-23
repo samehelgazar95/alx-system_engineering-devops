0f638f81ac52ff76664b9eb5664ac8fde2aa0455
Sameh ElGazar <selgazar95@gmail.com>
2024-02-24 00:40:10 +0200

    Task 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,12 +4,12 @@ package{ 'python3':
   ensure => '3.8.10'
 }
 
-package{ 'werkzeug':
-  ensure  => '2.1.1',
-}
-
 package{ 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+}
+
+package{ 'werkzeug':
+  ensure => '2.1.1'
 }
 
