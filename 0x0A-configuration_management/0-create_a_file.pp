c2dba7683e8b7c74d0dab119b0f43936a2e68a9c
Sameh ElGazar <selgazar95@gmail.com>
2024-02-23 23:51:08 +0200

    Task 1 puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,11 +1,11 @@
 # Creating file in /tmp/school
 
 file { '/tmp/school':
-  ensure  => file,
-  path    => '/tmp/school',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0774',
-  content => 'I love Puppet',
+ensure  => file,
+path    => '/tmp/school',
+owner   => 'www-data',
+group   => 'www-data',
+mode    => '0774',
+content => 'I love Puppet',
 }
 
