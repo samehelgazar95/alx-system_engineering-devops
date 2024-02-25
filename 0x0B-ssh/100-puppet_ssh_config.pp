e8fd19792b7a60d74315517ebae8a045653388dd
Sameh ElGazar <selgazar95@gmail.com>
2024-02-25 03:10:11 +0200

    Puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,15 @@
 # Puppet manifest to setup configs to ssh
+# >>>> NOT WORKING <<<<
 
 
 file_line {'Declare identity file':
     ensure => 'present',
-    path   => '~/.ssh/config'
+    path   => 'etc/ssh/ssh_config'
     line   => '     IdentityFile ~/.ssh/school',
 }
 
 file_line {'Turn off passwd auth':
     ensure => 'present',
-    path   => '~/.ssh/config',
-    line   => "     PasswordAuthentication no"
+    path   => 'etc/ssh/ssh_config',
+    line   => '     PasswordAuthentication no'
 }
