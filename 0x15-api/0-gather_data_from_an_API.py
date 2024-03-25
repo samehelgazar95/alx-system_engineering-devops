96bd69217f3b45881167acd3344e219ec79a7a3e
Sameh ElGazar <selgazar95@gmail.com>
2024-03-25 18:11:39 +0200

    Task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,13 +8,12 @@ if __name__ == "__main__":
     users_params = {'id': argv[1]}
     todos_params = {'userId': argv[1]}
     done_params = {'userId': argv[1], "completed": "true"}
+    users_url = "https://jsonplaceholder.typicode.com/users"
+    todos_url = "https://jsonplaceholder.typicode.com/todos"
 
-    users = requests.get('https://jsonplaceholder.typicode.com/users',
-                         params=users_params).json()
-    done_todos = requests.get('https://jsonplaceholder.typicode.com/todos',
-                              params=done_params).json()
-    all_todos = requests.get('https://jsonplaceholder.typicode.com/todos',
-                             params=todos_params).json()
+    users = requests.get(users_url, params=users_params).json()
+    done_todos = requests.get(todos_url, params=done_params).json()
+    all_todos = requests.get(todos_url, params=todos_params).json()
 
     name = users[0]["name"]
     done_len = len(done_todos)
@@ -24,4 +23,4 @@ if __name__ == "__main__":
         name, done_len, all_todos_len))
 
     for todo in done_todos:
-        print("\t{}".format(todo["title"]))
+        print("\t {}".format(todo["title"]))
