8c87c5bbcfa0f426966f29123018a853cc94e81c
Sameh ElGazar <selgazar95@gmail.com>
2024-04-09 22:53:32 +0200

    0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from requests import get
+"""
+returns the number of subscribers (not active users,
+total subscribers) for a given subreddit
+"""
+
+
+def number_of_subscribers(subreddit):
+    """
+    Fetching subreddit
+        Return: the number of subscribers (not active users, total subscribers)
+        for a given subreddit, if an invalid subreddit is given, return 0.
+    """
+    url = f'https://www.reddit.com/r/{subreddit}.json?'
+    try:
+        res = get(url, headers={'User-agent': 'elgazar'})
+        data = res.json()
+        return (data['data']['children'][0]['data']['subreddit_subscribers'])
+    except Exception as e:
+        return (0)
