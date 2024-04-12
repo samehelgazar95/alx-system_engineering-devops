caaa4df283d1070ae7c1ce1d7dfeb014483547eb
Sameh ElGazar <selgazar95@gmail.com>
2024-04-12 19:34:27 +0200

    0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 """
-Function that queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit.
-If an invalid subreddit is given, the function should return 0
+returns the number of subscribers (not active users,
+total subscribers) for a given subreddit
 """
-
-import requests
+from requests import get
 
 
 def number_of_subscribers(subreddit):
     """
-    Function that queries the Reddit API
-    - If not a valid subreddit, return 0.
+    Fetching subreddit
+        Return: the number of subscribers (not active users, total subscribers)
+        for a given subreddit, if an invalid subreddit is given, return 0.
     """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-    )
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    try:
+        res = get(url, headers={'User-agent': 'app/1.0'},
+                  allow_redirects=False)
 
-    if req.status_code == 200:
-        return req.json().get("data").get("subscribers")
-    else:
-        return 0
+        if res.status_code == 200:
+            data = res.json()
+            return data['data']['subscribers']
+    except Exception as e:
+        return (0)
--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,8 @@
 # replace 'phpp' with 'php' in 'wp-settings.php'
 
 exec{'fix-wordpress':
-	command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-	path 	=> '/var/local/bin/:/bin/'
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/var/local/bin/:/bin/'
+
 }
 
